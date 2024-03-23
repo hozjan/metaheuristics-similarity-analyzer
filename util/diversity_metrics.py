f10bb1d1bcd48b43cee827857b20bf3edb50b495
hozjan <truba.zan@gmail.com>
2024-03-23 14:17:38 +0100

    feat(diversity_metrics): added PED and PMD metrics

--- util/diversity_metrics.py
+++ util/diversity_metrics.py
@@ -3,18 +3,20 @@ import math
 from enum import Enum
 from niapy.problems import Problem
 
-__all__ = [
-    'PDC'
-]
+__all__ = ["PDC", "PED", "PMD"]
+
 
 class DiversityMetric(Enum):
-    PDC = 'pdc'
+    PDC = "pdc"
+    PED = "ped"
+    PMD = "pmd"
+
 
 def PDC(population, problem: Problem):
     r"""Distance to Population Centroid.
 
-    Reference paper: 
-        Ursem, Rasmus. (2002). Diversity-Guided Evolutionary Algorithms. 2439. 10.1007/3-540-45712-7_45. 
+    Reference paper:
+        Ursem, Rasmus. (2002). Diversity-Guided Evolutionary Algorithms. 2439. 10.1007/3-540-45712-7_45.
 
     Args:
         population (numpy.ndarray): population.
@@ -45,4 +47,48 @@ def PDC(population, problem: Problem):
             sum += math.pow(x - avg_point[j], 2)
         pdc += math.sqrt(sum)
 
-    return pdc / ( P * L)
+    return pdc / (P * L)
+
+
+def PED(population):
+    r"""Population Euclidean distance.
+
+    Args:
+        population (numpy.ndarray): population.
+
+    Returns:
+        PED value.
+
+    """
+    ped = 0
+
+    for index_i, pi in enumerate(population):
+        for pj in population[index_i + 1 :]:
+            sum = 0
+            for xi, xj in zip(pi, pj):
+                sum += math.pow(xi - xj, 2)
+            ped += math.sqrt(sum)
+
+    return ped
+
+
+def PMD(population):
+    r"""Population Manhattan distance.
+
+    Args:
+        population (numpy.ndarray): population.
+
+    Returns:
+        PMD value.
+
+    """
+    pmd = 0
+
+    for index_i, pi in enumerate(population):
+        for pj in population[index_i + 1 :]:
+            sum = 0
+            for xi, xj in zip(pi, pj):
+                sum += abs(xi - xj)
+            pmd += sum
+
+    return pmd
